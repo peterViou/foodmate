6c632752894fad00c44875549fbfd833872cafa9
Peter Duchastenier <peterduchastenier@gmail.com>
2024-06-13 11:47:01 -0600

    Update meal-detail component template and functionality

--- src/app/meal/meal-detail/meal-detail.component.ts
+++ src/app/meal/meal-detail/meal-detail.component.ts
@@ -1,29 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MealService, Meal } from '../meal.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-meal-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './meal-detail.component.html',
   styleUrls: ['./meal-detail.component.css'],
 })
 export class MealDetailComponent implements OnInit {
-  mealId!: string;
   meal$!: Observable<Meal | undefined>;
+  mealId!: string;
 
   constructor(
     private mealService: MealService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
-      this.mealId = params.get('id')!;
-      this.meal$ = this.mealService.getMeal(this.mealId);
+      this.mealId = params.get('id') || '';
+      if (this.mealId) {
+        this.meal$ = this.mealService.getMeal(this.mealId);
+      }
     });
+  }
+
+  deleteMeal(): void {
+    if (this.mealId) {
+      this.mealService
+        .deleteMeal(this.mealId)
+        .then(() => {
+          this.router.navigate(['/meal/list']);
+        })
+        .catch((error) => {
+          console.error('Error deleting meal:', error);
+        });
+    }
   }
 }
