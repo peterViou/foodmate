60c3c5155e0ba1e867e27bc6bb10ed9f687dbf47
Peter Duchastenier <peterduchastenier@gmail.com>
2024-06-13 22:24:24 -0600

    Add auth guard to meal routes

--- src/app/chat/chat-routing.module.ts
+++ src/app/chat/chat-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatComponent } from './chat.component';
+import { authGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: ChatComponent,
+    canActivate: [authGuard],
   },
 ];
 
--- src/app/meal/meal-routing.module.ts
+++ src/app/meal/meal-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { MealListComponent } from './meal-list/meal-list.component';
 import { MealDetailComponent } from './meal-detail/meal-detail.component';
 import { MealFormComponent } from './meal-form/meal-form.component';
+import { authGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
-  { path: 'list', component: MealListComponent },
-  { path: 'detail/:id', component: MealDetailComponent },
-  { path: 'add', component: MealFormComponent },
-  { path: 'edit/:id', component: MealFormComponent },
+  { path: 'list', component: MealListComponent, canActivate: [authGuard] },
+  {
+    path: 'detail/:id',
+    component: MealDetailComponent,
+    canActivate: [authGuard],
+  },
+  { path: 'add', component: MealFormComponent, canActivate: [authGuard] },
+  { path: 'edit/:id', component: MealFormComponent, canActivate: [authGuard] },
   { path: '', redirectTo: 'list', pathMatch: 'full' },
   { path: '**', redirectTo: 'list' },
 ];
